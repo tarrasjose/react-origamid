5fa735afbfd6a356c8fa9000d16d96823f0fe19c
tarrasjose <104996954+tarrasjose@users.noreply.github.com>
2023-04-22 17:40:13 -0300

    jsx arrays

---
+++ 08-jsx-arrays/appreact/src/App.js
@@ -0,0 +1,52 @@
+import React from "react";
+
+// const App = () => {
+//   const filmes = ['Before Sunrise', 'Before Sunset', 'Before Midnight'];
+
+//   return (
+//     <ul>
+//       {filmes.map((filme) => {
+//         return <li key={filme}>{filme}</li>
+//       })}
+//     </ul>
+//   );
+// };
+
+// const App = () => {
+//   const livros = [
+//     { nome: 'A Game of Thrones', ano: 1996 },
+//     { nome: 'A Clash of Kings', ano: 1998 },
+//     { nome: 'A Storm of Swords', ano: 2000 },
+//   ];
+
+//   return (
+//     <ul>
+//       {livros.map(({ nome, ano }) => (
+//         <li key={nome}>{nome}, {ano}</li>
+//       ))}
+//     </ul>
+//   );
+// };
+
+
+// Filtrando livros a partir de 1998
+const App = () => {
+  const livros = [
+    { nome: 'A Game of Thrones', ano: 1996 },
+    { nome: 'A Clash of Kings', ano: 1998 },
+    { nome: 'A Storm of Swords', ano: 2000 },
+  ];
+
+  return (
+    <ul>
+      {livros.filter(({ ano }) => ano >= 1998)
+      .map(({ nome, ano }) => (
+        <li key={nome}>
+          {nome}, {ano}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default App;
