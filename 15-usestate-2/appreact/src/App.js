6a5ba9db4a3d4744e8018c545764c88dcd36d958
Tárras José <tarras_souza@hotmail.com>
2023-05-30 18:41:02 -0300

    aula usestate 2

---
+++ 15-usestate-2/appreact/src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import ButtonModal from "./ButtonModal";
+import Modal from "./Modal";
+
+const App = () => {
+  const [modal, setModal] = React.useState(true);
+  const [items, setItems] = React.useState('Teste');
+
+  function handleClick() {
+    setItems('Outro');
+  }
+
+  return (
+    <div>
+      <p>{items}</p>
+      <button onClick={handleClick}>Clicar</button>
+      <Modal modal={modal} setModal={setModal} />
+      <ButtonModal setModal={ setModal }/>
+    </div>
+    
+  )
+};
+
+
+
+
+export default App;
---
+++ 15-usestate-2/appreact/src/ButtonModal.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ButtonModal = ({ setModal }) => {
+  function handleClick() {
+    setModal((ativo) => !ativo)
+  }
+
+  return (
+    <button onClick={handleClick}>Abrir</button>
+  )
+}
+
+export default ButtonModal
