988b0c16c9d0541a85c17145aed028492c7f2c1f
Tárras José <tarras_souza@hotmail.com>
2023-05-30 19:08:34 -0300

    usestate exercicio

---
+++ 17-usestate-3-exercicio/appreact/src/Produto.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+// const Produto = ({ dados }) => {
+//   return (
+//     <div>
+//       <h1>{dados.nome}</h1>
+//       <p>R$ {dados.preco}</p>
+//       <img style={{width: '300px'}} src={dados.fotos[0].src} alt={dados.fotos[0].titulo} />
+//     </div>
+//   )
+// }
+
+// export default Produto;
+
+
+///////////////////////////////
+//Treinamento
+
+const Produto = ({ dados }) => {
+
+  return (
+    <div>
+      <h1>{dados.nome}</h1>
+      <p>R$ {dados.preco}</p>
+      <img src={dados.fotos[0].src} alt={dados.fotos[0].titulo} />
+    </div>
+  );
+}
+
+export default Produto;
